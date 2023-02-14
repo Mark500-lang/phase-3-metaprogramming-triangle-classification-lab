9389bf796cbe0f6f6a173b8dea49cf229c8ee12b
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-02-14 07:00:39 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  attr_accessor :kind
+
+  def initialize(l1, l2, l3)
+    @l1=l1
+    @l2=l2
+    @l3=l3
+
+    if l1 <= 0 || l2 <= 0 || l3 <= 0 || l1+l2<=l3 || l2+l3<=l1 || l1+l3<=l2
+      raise TriangleError
+    elsif l1==l2 && l2==l3
+      @kind = :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+     "Invalid sizes"
+    end
+  end
 end
